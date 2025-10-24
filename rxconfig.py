016779f0d997ebfc39fad20132b5a18521528865
Gateston Johns <gateston136@gmail.com>
2025-10-24 12:22:39 -0400

    fixed routing

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="sqllm",
+    frontend_path="/SQLLM",  # Base path for the frontend
+    api_url="https://sbaai.sbasite.com/SQLLM_API",  # Update with your backend URL
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
